7e31eb719c3f9ce25fddce5cdbf72bec52dbd6fc
Pedro-h-mattos <pedro.h.mattos@outlook.com>
2026-04-28 17:34:12 +0100

    reformatted code

--- RemoveEmptyModule.psm1
+++ RemoveEmptyModule.psm1
@@ -1,47 +1,46 @@
 <#
-.SYNOPSIS
-	Recursively removes empty subdirectories from a specified root directory.
+	.SYNOPSIS
+		Recursively removes empty subdirectories from a specified root directory.
 
-.DESCRIPTION
-	This script traverses a directory tree recursively and deletes empty subdirectories (containing no files or folders).
-	
-	Child directories are processed first, so that directories containing empty subdirectories are also removed.
+	.DESCRIPTION
+		This script traverses a directory tree recursively and deletes empty subdirectories (containing no files or folders).
+		
+		Child directories are processed first, so that directories containing empty subdirectories are also removed.
 
-.PARAMETER InputPath
-	Specifies the root directory (to be processed).
+	.PARAMETER InputPath
+		Specifies the root directory (to be processed).
 
-.PARAMETER RootPath
-	Internal parameter, used to track the root directory
+	.PARAMETER RootPath
+		Internal parameter, used to track the root directory
 
-.EXAMPLE
-	Remove-Empty -Path "C:\foo\"
+	.EXAMPLE
+		Remove-Empty -Path "C:\foo\"
 
-.NOTES
-	Version:		1.0
-	Author:			Pedro Henrique Mattos
-	Creation Date:	23.04.2026
+	.NOTES
+		Version:		1.0
+		Author:			Pedro Henrique Mattos
+		Creation Date:	23.04.2026
 #>
-function Remove-Empty {
+function Remove-Empty{
 	[CmdletBinding()]
-    param(
-		[Parameter(Mandatory = $true)]
+	param(
+		[Parameter(Mandatory = $true, HelpMessage = "Path to the root directory")] 
 		[string]$Path,
 		
-		[string]$RootPath
-    )
+		[string]$Rootpath
+	)
 	
-	if (-not $RootPath) {
-		$RootPath = $Path
-		Write-Verbose "Processing subdirectories in root: $RootPath"
-	}
-	
-    foreach ($Dir in Get-ChildItem -Force -LiteralPath $Path -Directory -ErrorAction SilentlyContinue) {
-        Remove-Empty -Path $Dir.FullName -RootPath $RootPath
-    }
-	
-    if ($Path -ne $RootPath -and -not (Get-ChildItem -Force -LiteralPath $Path -ErrorAction SilentlyContinue)) {
-		$RelativePath = [System.IO.Path]::GetRelativePath((Resolve-Path $RootPath), (Resolve-Path $Path))
-		Remove-Item -Force -LiteralPath $Path
-		Write-Verbose ("Removed: {0}{1}" -f $RootPath, $RelativePath) 
+	process {
+		if (-not $RootPath) {
+			$Rootpath = $Path
+		}
+		
+		foreach ($Dir in Get-ChildItem -Force -LiteralPath $Path -Directory -ErrorAction SilentlyContinue) {
+			Remove-Empty -Path $Dir.FullName -RootPath $RootPath
+		}
+		
+		if ($Path -ne $RootPath -and -not (Get-ChildItem -Force -LiteralPath $Path -ErrorAction SilentlyContinue)) {
+			Remove-Item -Force -LiteralPath $Path
+		} 
 	}
 }
